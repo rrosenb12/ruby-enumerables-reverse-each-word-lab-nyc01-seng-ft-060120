f2d68026453fb9e2e686c30534161c878ff3f469
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-13 22:42:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,5 @@
 def reverse_each_word(string)
   array = string.split
-  reversed_array = []
-  array.each do |word|
-    reversed_array << word.reverse
-  end
-  reversed_array.join(" ")
-end
-
-def reverse_each_word(string)
-  array = string.split
   array.collect! do |string|
     string.reverse
   end
